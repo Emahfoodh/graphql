48971a4305f275d98c1aefaa5fd6c8375192d2a9
emahfoodh <eman.mjm@gmail.com>
2024-06-02 17:49:46 +0300

    fix

--- src/pages/dashboard/components/Table.tsx
+++ src/pages/dashboard/components/Table.tsx
@@ -5,9 +5,6 @@ interface TableProps {
 }
 
 const Table: React.FC<TableProps> = ({ transactions }) => {
-  transactions = transactions.slice().sort((a: Transaction, b: Transaction) => {
-    return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-  });
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
@@ -33,32 +30,40 @@ const Table: React.FC<TableProps> = ({ transactions }) => {
           </div>
         </div>
         {transactions &&
-          transactions.map((transaction, key) => (
-            <div
-              className={`grid grid-cols-3 sm:grid-cols-3 ${
-                key === transactions.length - 1
-                  ? ''
-                  : 'border-b border-stroke dark:border-strokedark'
-              }`}
-              key={key}
-            >
-              <div className="flex items-center gap-3 p-2.5 xl:p-5">
-                <p className="hidden text-black dark:text-white sm:block">
-                  {transaction.object.name}
-                </p>
-              </div>
+          transactions
+            .slice()
+            .sort((a: Transaction, b: Transaction) => {
+              return (
+                new Date(b.createdAt).getTime() -
+                new Date(a.createdAt).getTime()
+              );
+            })
+            .map((transaction, key) => (
+              <div
+                className={`grid grid-cols-3 sm:grid-cols-3 ${
+                  key === transactions.length - 1
+                    ? ''
+                    : 'border-b border-stroke dark:border-strokedark'
+                }`}
+                key={key}
+              >
+                <div className="flex items-center gap-3 p-2.5 xl:p-5">
+                  <p className="hidden text-black dark:text-white sm:block">
+                    {transaction.object.name}
+                  </p>
+                </div>
 
-              <div className="flex items-center justify-center p-2.5 xl:p-5">
-                <p className="text-meta-3">{transaction.amount/1000}KB</p>{' '}
-              </div>
+                <div className="flex items-center justify-center p-2.5 xl:p-5">
+                  <p className="text-meta-3">{transaction.amount / 1000}KB</p>{' '}
+                </div>
 
-              <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
-                <p className="text-meta-5">
-                  {new Date(transaction.createdAt).toLocaleString()}
-                </p>{' '}
+                <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+                  <p className="text-meta-5">
+                    {new Date(transaction.createdAt).toLocaleString()}
+                  </p>{' '}
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
       </div>
     </div>
   );
